32a5509b573756dd86c10b4152af1345f32413a2
liutao <gg.cn.lt@gmail.com>
2023-09-07 16:36:05 +0800

    add runtime

--- src/WorkermanHttpServer.php
+++ src/WorkermanHttpServer.php
@@ -4,21 +4,26 @@ declare(strict_types=1);
 
 namespace MicroPHP\Workerman;
 
-use League\Route\Router;
 use League\Route\Strategy\ApplicationStrategy;
 use MicroPHP\Framework\Application;
-use MicroPHP\Framework\Http\Contract\ServerInterface;
+use MicroPHP\Framework\Config\Config;
+use MicroPHP\Framework\Http\Contract\HttpServerInterface;
 use MicroPHP\Framework\Http\ServerConfig;
+use MicroPHP\Framework\Http\Traits\HttpServerTrait;
+use MicroPHP\Framework\Router\Router;
 use Workerman\Connection\TcpConnection;
 use Workerman\Protocols\Http\Request;
 use Workerman\Psr7\Response;
 use Workerman\Psr7\ServerRequest;
 use Workerman\Worker;
 
-class WorkermanHttpServer implements ServerInterface
+class WorkermanHttpServer implements HttpServerInterface
 {
+    use HttpServerTrait;
+
     public function run(Router $router): void
     {
+        $this->setRuntime();
         $strategy = new ApplicationStrategy();
         $strategy->setContainer(Application::getContainer());
         $router->setStrategy($strategy);
@@ -30,12 +35,27 @@ class WorkermanHttpServer implements ServerInterface
 
         $httpWorker->onMessage = function (TcpConnection $connection, Request $request) use ($router) {
             $psr7Request = new ServerRequest($request->rawBuffer());
-            $serverRequest = \MicroPHP\Framework\Http\ServerRequest::fromPsr7($psr7Request);
 
-            $response = $router->dispatch($serverRequest);
+            $response = $this->routeDispatch($router, \MicroPHP\Framework\Http\ServerRequest::fromPsr7($psr7Request));
             $connection->send(new Response($response->getStatusCode(), $response->getHeaders(), $response->getBody(), $response->getProtocolVersion(), $response->getReasonPhrase()));
         };
 
         Worker::runAll();
+    }
+
+    private function setRuntime(): void
+    {
+        $this->createRuntimeDir();
+        $enableWorkermanLog = Config::get('log.enable_workerman_log');
+        Worker::$logFile = base_path('runtime/logs/workerman.log');
+        if (empty($enableWorkermanLog) && $this->isLinux()) {
+            Worker::$logFile = '/dev/null';
+        }
+        Worker::$pidFile = 'runtime/' . uniqid('workerman_', true) . '.pid';
+    }
+
+    private function isLinux(): bool
+    {
+        return PHP_OS_FAMILY === 'Linux';
     }
 }
